1fdafa6b171dd8af52cfe356051c2ee00517d693
JEIO <ilsangsw@gmail.com>
2026-01-14 13:58:52 +0900

    빌드 오류 수정: Icons.tsx size prop을 style로 변경

--- app/apps/정보-보호의-성/components/Icons.tsx
+++ app/apps/정보-보호의-성/components/Icons.tsx
@@ -39,5 +39,5 @@ interface IconProps {
 
 export const DynamicIcon: React.FC<IconProps> = ({ name, className, size = 24 }) => {
   const IconComponent = IconMap[name] || Shield;
-  return <IconComponent className={className} size={size} />;
+  return <IconComponent className={className} style={{ width: size, height: size }} />;
 };
