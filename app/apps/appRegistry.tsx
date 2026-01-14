0e76f52bb6762e359bd7ff147fa316f931227b6a
JEIO <ilsangsw@gmail.com>
2026-01-14 20:51:20 +0900

    정보-보안-가디언-아카데미 앱 완전 삭제
    - 앱 폴더 삭제 (13개 파일)
    - appRegistry.tsx에서 등록 제거
    - data/apps.ts에서 앱 데이터 제거

--- app/apps/appRegistry.tsx
+++ app/apps/appRegistry.tsx
@@ -29,7 +29,6 @@ const appComponents: Record<string, () => Promise<{ default: AppComponent }>> = 
   '우리-학교-저작권-지킴이-ccl-maker': () => import('./우리-학교-저작권-지킴이-ccl-maker/App'),
   '위협-데이터베이스': () => import('./위협-데이터베이스/App'),
   '저작권과-공유의-세계': () => import('./저작권과-공유의-세계/App'),
-  '정보-보안-가디언-아카데미': () => import('./정보-보안-가디언-아카데미/App'),
   '정보-보안-지킴이': () => import('./정보-보안-지킴이/App'),
   '정보-보호의-성': () => import('./정보-보호의-성/App'),
 }
